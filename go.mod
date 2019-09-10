c663c2c7fbab064fbda819f081f659faf5c700cf
marwan-at-work <marwan.sameer@gmail.com>
2019-09-10 15:54:30 -0400

    init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/gomvs/a
+
+go 1.13
+
+require (
+	github.com/gomvs/b v1.2.0
+	github.com/gomvs/c v1.2.0
+)
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gomvs/b"
+	"github.com/gomvs/c"
+)
+
+func main() {
+	fmt.Println("A")
+	b.Hello()
+	c.Hello()
+}
